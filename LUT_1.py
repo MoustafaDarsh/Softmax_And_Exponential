4152b01f0a0fee1231002035c865c1cc52d941a6
Moustafa Wassim <moustafawassimabdelaziz@gmail.com>
2023-08-08 06:23:19 +0300

    Generation Scripts of LUTs used in Exponential

--- LUT_1.py
+++ LUT_1.py
@@ -19,5 +19,3 @@ with open(file_name,'a') as f:
     for i in range (len(exp_values_binary)-1):
         f.write(f'''{exp_values_binary[i]}\n''')
     f.write(f'''{exp_values_binary[i+1]}''')
-
-    
